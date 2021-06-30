19b41db66a7c9341e2a83778b94b6712926a570f
Ilja Robalkin <irobalkin@interactivebrokers.com>
2021-06-30 19:18:28 +0300

    Removes non-used imports

--- src/main/java/ee/ilja/evaluation/controller/LoginController.java
+++ src/main/java/ee/ilja/evaluation/controller/LoginController.java
@@ -1,11 +1,9 @@
 package ee.ilja.evaluation.controller;
 
-import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.naming.AuthenticationException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
